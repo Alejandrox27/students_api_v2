d31196f93b8a9f323ae9908fa6372b9be8970fe1
alx_27 <didieralejandromejia83@gmail.com>
2023-12-22 12:25:47 -0500

    get content from html

---
+++ script.js
@@ -0,0 +1,5 @@
+const formulario = document.getElementById("formulario");
+const estudiante = document.getElementById("templateEstudiante");
+const profesor = document.getElementById("templateProfesor");
+const cardsEstudiantes = document.getElementById("cardsEstudiantes");
+const cardsProfesores = document.getElementById("cardsProfesores");
